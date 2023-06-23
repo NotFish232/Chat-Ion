603bc4c6b629d162ed6a4d3fe53c421e8023d6b5
Justin Lee <2025jlee@tjhsst.edu>
2023-06-23 19:58:04 -0400

    toy example using transformer

--- network.py
+++ network.py
@@ -1,6 +1,8 @@
 import torch as T
 from torch import nn
+from torch.nn import functional as F
 from typing_extensions import Self
+import math
 
 
 class PositionalEncoding(nn.Module):
@@ -12,9 +14,8 @@ class PositionalEncoding(nn.Module):
         self.embed_dim = embed_dim
 
         self.encoding = T.zeros((num_embed, embed_dim))
-        self.dropout = nn.Dropout2d()
+        self.dropout = nn.Dropout1d()
 
-   
         positions = T.arange(num_embed).unsqueeze(1).repeat(1, embed_dim // 2)
         div_terms = (
             (10_000 ** (T.arange(0, embed_dim, 2) / embed_dim))
@@ -25,23 +26,45 @@ class PositionalEncoding(nn.Module):
         self.encoding[:, 1::2] = T.cos(positions / div_terms)
 
     def forward(self: Self, x: T.Tensor) -> T.Tensor:
-        return self.dropout(x + self.encoding[:x.size(0)])
+        return self.dropout(x + self.encoding[: x.size(0)])
 
 
 class Network(nn.Module):
     def __init__(self: Self, num_embed: int, embed_dim: int) -> None:
-        self.embeddings = nn.Sequential(
-            nn.Embedding(num_embed, embed_dim),
-        )
-        self.transformer = nn.Transformer()
+        super().__init__()
 
-    def forward(self: Self, x: T.Tensor) -> T.Tensor:
-        pass
+        self.embed_dim = embed_dim
+
+        self.embedding = nn.Embedding(num_embed, embed_dim)
+        self.positional_encoding = PositionalEncoding(num_embed, embed_dim)
+        self.transformer = nn.Transformer(embed_dim, batch_first=True)
+        self.linear = nn.Linear(embed_dim, num_embed)
+
+    def forward(self: Self, src: T.Tensor, tgt: T.Tensor) -> T.Tensor:
+        src = self.embedding(src) * math.sqrt(self.embed_dim)
+        tgt = self.embedding(tgt) * math.sqrt(self.embed_dim)
+
+        src = self.positional_encoding(src)
+        tgt = self.positional_encoding(tgt)
+
+        x = self.transformer(src, tgt)
+
+        x = self.linear(x)
+
+        if not self.training:
+            x = F.softmax(x, dim=1)
+
+        return x
 
 
 def main() -> None:
-    p = PositionalEncoding(100, 10_000)
-    print(p.embeddings)
+    n = Network(5, 16)
+    n.eval()
+    print(sum(i.numel() for i in n.parameters()))
+    x = T.randint(0, 5, (1, 5))
+    y = T.randint(0, 5, (1, 5))
+    with T.no_grad():
+        print(x, y, T.argmax(n(x, y), dim=1), sep = '\n')
 
 
 if __name__ == "__main__":
