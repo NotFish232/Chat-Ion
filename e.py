b7daa804c7392209c0521b3a837b930eae901663
Justin Lee <2025jlee@tjhsst.edu>
2023-06-23 19:59:23 -0400

    del misc file

--- e.py
+++
@@ -1,7 +0,0 @@
-if user_agent and not any(
-            (
-                any(user_agent_substring in user_agent for user_agent_substring in settings.NONLOGGABLE_USER_AGENT_SUBSTRINGS),
-                any(map(path.startswith,settings.NONLOGGABLE_PATH_BEGINNINGS)),
-                any(path.endswith(settings.NONLOGGABLE_PATH_ENDINGS))),
-            )
-        ):
