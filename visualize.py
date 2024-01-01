9cd1979831ba9909e1791811968c23f7f6e7a4f0
Justin Lee <2025jlee@tjhsst.edu>
2024-01-01 15:16:51 -0500

    config tensorboard

--- visualize.py
+++ visualize.py
@@ -1,12 +1,18 @@
-from torch.utils.tensorboard import SummaryWriter
-from tensorboard import program
+import os
+import tensorboard
+import time
 
 RUNS_DIR = "tensorboard_runs/"
 
-#writer = SummaryWriter(RUNS_DIR)
 
-if __name__ == "__main__":
-    tb = program.TensorBoard()
+def main() -> None:
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+    tb = tensorboard.program.TensorBoard()
     tb.configure(argv=[None, '--logdir', RUNS_DIR])
     url = tb.launch()
-    print(f"Tensorflow listening on {url}")
+    print(f"Running at \"{url}\"")
+    while True:
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
